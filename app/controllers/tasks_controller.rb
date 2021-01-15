ef37e4179ee98f3d1f9ed6c76efe786b91cf362a
jacklos <ec2-user@ip-172-31-60-124.ec2.internal>
2021-01-15 01:28:18 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,12 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in, except: [:index]
+  
   
   def index
-    @tasks = Task.all.page(params[:page])
+    if logged_in?
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
@@ -13,7 +17,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に登録されました'
