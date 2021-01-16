7933e91a190fb349ed2e32147a50cc99acd51d72
jacklos <ec2-user@ip-172-31-60-124.ec2.internal>
2021-01-16 03:01:41 +0000

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_130301) do
+ActiveRecord::Schema.define(version: 2021_01_16_023226) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
